1f3c2e62b6b6a5b8aa60090c495e9a77cc00da88
David Popovici <davidpopovici24@gmail.com>
2025-04-21 14:35:29 -0400

    created main.js and added notes

---
+++ main.js
@@ -0,0 +1,46 @@
+// var, let, const
+// var is rarely used anymore, only use let and const
+// let allows the variable to change in value
+// const is a static variable that can not change
+
+// String, Numbers, Boolean, null, undefined
+
+//const name = "John"; String
+//const age = 30; Number
+//const rating = 4.5; Number
+//const isCool = true; Boolean
+//const x = null; Outputs on console as object but is actually null
+//const y = undefined; Undefined
+//let z; Also undefined if there is no value assigned
+
+//Concatenation
+//console.log("My name is " + name +" and I am " + age);
+// Template String
+//const hello = `My name is ${name} and I am ${age}`;
+
+
+//String
+
+//const s = "Hello World!";
+//console.log(s.length); Gives the length of a string
+//console.log(s.toUpperCase()); Capitalizes everything in String
+//console.log(s.toLowerCase()); Makes everything in String Lowercase
+//console.log(s.substring(0, 5)); Pulls out only certain portion of the String
+//console.log(s.substring(0, 5).toUpperCase()); Able to Chain together 
+
+//const s = "technology, computers, it, code";
+//console.log(s.split(", ")); Useful for tag system to split words in 1 variable
+
+
+//Arrays - variables that hold multiple values
+//Arrays are 0 based which means it always starts with 0,1,2, and so on
+
+const fruits = ["apples", "oranges", "pears",];
+
+fruits[3] = "grapes";
+
+console.log(fruits);
+
+
+
+
